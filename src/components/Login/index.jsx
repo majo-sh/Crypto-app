bbb2895386d2d01895d2aecc56ce41d12395a318
majo-sh <majo.cortes.sh@gmail.com>
2022-03-08 19:03:52 -0500

    Correccion de la url de consulta para login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -4,7 +4,7 @@ import "./Login.scss"
 import Cookies from "universal-cookie";
 import {useNavigate} from "react-router-dom"
 
-const baseUrl = "https://majo-sh.github.io/json-server-fake-api/db.json"
+const baseUrl = "https://majo-sh.github.io/json-server-fake-api/db.json/usuarios"
 const cookies = new Cookies()
 
 export default function Login () {
